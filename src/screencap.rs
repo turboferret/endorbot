2429983aff1f9bb3898b15594ad02fe612e99246
erik <engammalgrek@gmail.com>
2026-01-21 13:12:55 +0100

    A

--- src/screencap.rs
+++ src/screencap.rs
@@ -165,9 +165,9 @@ fn find_text_char(x:u32, y:u32, image:&DynamicImage, opt:&Opt) -> TextChar {
         println!("\tCheck 8");
     }
     if get_pixel(image, x, y, x, y + 1, opt) == clr
-        && get_pixel(image, x, y, x - 3, y + 5, opt) == clr
-            && get_pixel(image, x, y, x + 6, y + 5, opt) == clr
-            && get_pixel(image, x, y, x + 7, y + 16, opt) == clr
+        && (get_pixel(image, x, y, x - 3, y + 5, opt) == clr || get_pixel(image, x, y, x - 5, y + 5, opt) == clr)
+            && (get_pixel(image, x, y, x + 6, y + 5, opt) == clr || get_pixel(image, x, y, x + 4, y + 5, opt) == clr)
+            && (get_pixel(image, x, y, x + 7, y + 16, opt) == clr || get_pixel(image, x, y, x + 5, y + 16, opt) == clr)
             && get_pixel(image, x, y, x - 4, y + 19, opt) == clr {
         return TextChar::Digit(8);
     }
